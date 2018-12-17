c994411fa3849e98a8ea18fc564ec764e219f115
Brett Stewart <brett@wovn.io>
2018-12-17 10:43:45 +0900

    init

---
+++ CSharpScratchspace.Tests/ExampleTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace CSharpScratchspace.Tests
+{
+    public class ExampleTests
+    {
+        [Test]
+        public void TestCase()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ CSharpScratchspace/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CSharpScratchspace
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var json = JsonConvert.SerializeObject(new { TestJsonObj = "test" });
+
+            Console.WriteLine(json);
+        }
+    }
+}
